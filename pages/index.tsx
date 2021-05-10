0b59bf6d2a33e16df759b662106611c5670ac76d
Mimu-salon <tmimura0@gmail.com>
2021-05-11 00:28:53 +0900

    ✨ implement login function

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,36 @@
-import React, { memo, VFC } from 'react';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChangeEvent, memo, useState, VFC } from 'react';
+import { Box, ChakraProvider, Divider, Flex, Heading, Input, Stack } from '@chakra-ui/react';
 
 import theme from '../components/theme/theme';
+import { PrimaryButton } from '../components/atomic/atoms/button/PrimaryButton';
+import { useAuth } from '../components/hooks/useAuth';
 
 const Login: VFC = memo(() => {
+  const { login, loading } = useAuth();
+
+  const [userId, setUserId] = useState('');
+
+  const onChangeUserId = (e: ChangeEvent<HTMLInputElement>) => setUserId(e.target.value);
+
+  const onClickLogin = () => login(userId);
+
   return (
     <ChakraProvider theme={theme}>
-      <p>Loginページです</p>
+      <Flex align='center' justify='center' h='100vh'>
+        <Box bg='white' w='sm' p={4} borderRadius='md' shadow='md'>
+          <Heading as='h1' size='lg' textAlign='center'>
+            医師情報管理ツール
+          </Heading>
+          <Divider my={4} />
+          <Stack spacing={6} py={4} px={10}>
+            <Input placeholder='ユーザーID' value={userId} onChange={onChangeUserId} />
+            <Input placeholder='パスワード' />
+            <PrimaryButton disabled={userId === ''} loading={loading} onClick={onClickLogin}>
+              ログイン
+            </PrimaryButton>
+          </Stack>
+        </Box>
+      </Flex>
     </ChakraProvider>
   );
 });
